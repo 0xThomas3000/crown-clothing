ba31975025af8973345c94d9c47f3cf1ad0ef4ae
tuanit168 <tuanit168@gmail.com>
2021-10-28 01:15:56 +1100

    72. Styling Menu Items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => ( // Destructure "title" out of props
-  <div style={{
-    backgroundImage: `url(${imageUrl})` // dynamically make styles on specific components
-  }}
-    className={`${size} menu-item`}>
+  <div className={`${size} menu-item`}>
+    <div 
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})` // dynamically make styles on specific components
+      }}
+    />
     <div className='content'>
-      <h1 className='title'>{title}</h1>
+      <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
